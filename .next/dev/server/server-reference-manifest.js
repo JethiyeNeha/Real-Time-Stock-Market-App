51a3c703ccc02f87071b9b83c539af86567e2952
JethiyeNeha <neha.lachuria@gmail.com>
2025-10-23 17:08:30 +0530

    Stop tracking .next directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DPSADdxXcvk2HxWkwuL8oNlNaSYJ/8S92tT4UYOCNpA=\"\n}"
--- .next/dev/static/chunks/app_(root)_layout_tsx_7f368c50._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_29f66a8c._.js",
-  "static/chunks/_f0496bdc._.js"
-],
-    source: "dynamic"
-});
